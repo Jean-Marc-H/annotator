85447b84a8f85ca7fdc65ebeb7ea29f3fe1b98bd
Unknown <JeanMarc.Hebert1996@gmail.com>
2018-12-04 11:37:26 -0500

    Fix bug #93
    Ajout de vérification de courriel valide

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -3,16 +3,52 @@ import { AuthService } from '../shared/security/auth.service';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.css']
+  //styleUrls: ['./reset-password.component.css']
+<<<<<<< HEAD
+=======
 })
 export class ResetPasswordComponent implements OnInit {
   userInfo: any = {};
   constructor(public auth: AuthService,) { }
+  errorMessage='';
 
   ngOnInit() {
 
   }
   resetPassword(email: string) {
-   this.auth.resetPassword(this.userInfo.email);
+   this.auth.resetPassword(this.userInfo.email).then().catch((error)=>{console.log(error);this.errorMessage=error.message})
+  }
+  emailInvalid(){
+	return this.errorMessage !== '';
   }
 }
+/*import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/security/auth.service';
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.css']
+>>>>>>> master
+})
+export class ResetPasswordComponent implements OnInit {
+  userInfo: any = {};
+  constructor(public auth: AuthService,) { }
+  errorMessage='';
+
+  ngOnInit() {
+
+  }
+  resetPassword(email: string) {
+   this.auth.resetPassword(this.userInfo.email).then()
+   .catch((error)=>{
+	   console.log(error);
+		this.errorMessage=error.message})
+  }
+<<<<<<< HEAD
+  emailInvalid(){
+	return this.errorMessage !== '';
+  }
+}
+=======
+}*/
+>>>>>>> master
